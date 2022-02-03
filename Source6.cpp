0064061ba8fc916afc1dd2c78e4ab3f4ffe133a5
z20ele25 <z20ele25@z00p3-11.SPSKNM>
2022-02-03 08:04:55 +0100

    Create Source6.cpp

---
+++ Source6.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char pole[100];
+    int i; int cisla = 0, medzery = 0;
+    int samohlasky = 0, spoluhlasky = 0;
+    cout << "Napis co ta napadne: \n";
+    gets_s(pole);
+    for (i = 0; pole[i] != '\0'; i++) {
+        if (pole[i] == 'a' || pole[i] == 'e' || pole[i] == 'i' || pole[i] == 'o' || pole[i] == 'u' ||
+            pole[i] == 'A' || pole[i] == 'E' || pole[i] == 'I' || pole[i] == 'O' || pole[i] == 'U') {
+            samohlasky++;
+        }
+        else if (
+            (pole[i] >= 'a' && pole[i] <= 'z' || pole[i] >= 'A' && pole[i] <= 'Z')) {
+            spoluhlasky++;
+        }
+        else if (pole[i] >= '0' && pole[i] <= '9')
+        {
+            cisla++;
+        }
+        else if (pole[i] == ' ') {
+            medzery++;
+        }
+    }
+    system("CLS");
+    cout << "\nZadane pole: " << pole;
+    cout << "\nPocet samohlasok: " << samohlasky;
+    cout << "\nPocet spoluhlasok: " << spoluhlasky;
+    cout << "\nPocet cisel: " << cisla;
+    cout << "\nPocet medzier: " << medzery;
+    return 0;
+}
